d142ee810a5567a22924173bd67e9d42ca3a6172
Preston Fulford <42225207+fulfordpr@users.noreply.github.com>
2021-07-27 16:26:13 -0400

    getting more functionality

--- app.js
+++ app.js
@@ -1,10 +1,100 @@
 var mainId = document.querySelector('#mainId')
 var lineId = document.querySelector('#tamaLine');
-var lineIdValue = lineId.selectedOptions[lineId.selectedIndex].value;
+var starDrumType = document.querySelector('#starDrumType');
+var snareSize = document.getElementById('snareSize');
+var kDrumSize = document.getElementById('kDrumSize');
+var inlay = document.getElementById('inlay');
+// var lineIdValue = lineId.selectedOptions[lineId.selectedIndex].value;
 
-// mainId.innerText = 'o';
-if (lineIdValue == 'tamaStarBubinga'){
-    mainId.innerText = 'TB';
-}else if (lineIdValue == 'tamaStarMaple'){
-    mainId.innerText = 'TM';
-}
+
+// SELCTING THE TAMA SERIES YOU WANT TO MAKE ###############
+lineId.addEventListener('change', (event) =>{
+    mainId.textContent = ''
+    const value = event.target.value;
+    let newText = ''
+    if (value == 'tamaStarBubinga'){
+        newText = 'TB';
+    }else if (value == 'tamaStarMaple'){
+        newText = 'TM';
+    }else if (value == 'tamaStarWal'){
+        newText = 'TW';
+    }
+    mainId.textContent += newText;
+
+});
+
+
+// CHOOSING THE TYPE OF DRUM YOU WANT TO MAKE ########
+starDrumType.addEventListener('change', (event) =>{
+    const value = event.target.value;
+    //RESETS THE DISPLAY OF THE SELECT BOXES
+    snareSize.style.display = "none";
+    kDrumSize.style.display = "none";
+    let newText = ''
+ 
+    // ADDING TO ITEMID AND MAKING THE SIZE BOX
+    //AVAILABLE FOR THE PARTICULAR TYPE OF DRUM
+    if (value == 'sDrum'){
+        newText = 'S';
+        snareSize.style.display = "inline";
+    }else if (value == 'kDrum'){
+        newText = 'B';
+        kDrumSize.style.display = "inline";
+    }else if (value == 'fTom'){
+        newText = 'F';
+    }else if (value == 'rTom'){
+        newText = 'T';
+    }
+
+
+    if (mainId.textContent.length == 2){
+        mainId.textContent += newText;
+    }   
+});
+
+
+snareSize.addEventListener('change', (event) =>{
+    const value = event.target.value;
+    let newText = ''
+
+    inlay.style.display = "none";
+
+    if (value == '136'){
+        newText = '136';
+        inlay.style.display = "inline";
+    }else if (value == '1455'){
+        newText = '1455';
+        inlay.style.display = "inline";
+    }else if (value == '1465'){
+        newText = '1465';
+        inlay.style.display = "inline";
+    }else if (value == '148'){
+        newText = '148';
+        inlay.style.display = "inline";
+    }
+
+
+    if (mainId.textContent.length == 3){
+        mainId.textContent += newText;
+    } 
+});
+
+inlay.addEventListener('change', (event) =>{
+    const value = event.target.value;
+    let newText = ''
+
+
+    if (value == 'd'){
+        newText = 'D';
+    }else if (value == 's'){
+        newText = 'S';
+    }else if (value == 'n'){
+        newText = '';
+    }
+    
+    
+    if (mainId.textContent.length >= 6){
+        mainId.textContent += newText;
+    } 
+
+});
